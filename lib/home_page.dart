42c95b0f695979acf01a33a79547a54998b9f456
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-01-29 12:09:23 +0530

    final-commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,9 +7,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title of the Question'),
+        title: const Text('BorderSide Widget'),
       ),
-      body: const SizedBox.shrink(),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: const BoxDecoration(
+          border: Border(
+             top: BorderSide(color: Colors.red, width: 10.0, style: BorderStyle.solid),
+          bottom: BorderSide(color: Colors.red, width: 10.0, style: BorderStyle.solid),
+            right: BorderSide(color: Colors.red, width: 5.0, style: BorderStyle.solid),
+            left: BorderSide(color: Colors.red, width: 5.0, style: BorderStyle.solid),
+            ),
+          ),
+          child:const Text("TutorialsPoint",style: TextStyle(fontSize: 32)),
+        ),
+      ),
     );
   }
 }
