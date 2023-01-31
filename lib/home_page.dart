d6c918189841405ed85bd0ded91f08dc5f13f349
Azhar <marmuazhar@gmail.com>
2023-02-01 00:46:35 +0530

    alert dialog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,7 +9,57 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tutorials Point'),
       ),
-      body: const SizedBox.shrink(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Alert Dialog Box'),
+                    content: const Text('This is an Alert Dialog Box'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop();
+                        },
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.black12,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          child: const Text('OK'),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                decoration: const BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Icon(Icons.info),
+                    SizedBox(width: 12),
+                    Text('Click here to see the dialog box'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
