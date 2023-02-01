aff34acddbba39019ea71841cf874f7091143de6
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-02-01 14:51:05 +0530

    commit_q20_clipRect_Widget

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tp_article
 
-article_link: 
+article_link: - https://docs.google.com/document/d/1SKqSxn6qTALDQGsiQmsrLQ72WMlWxc_IVhFLwDpbu1g/edit?usp=sharing
+
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,9 +7,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tutorials Point'),
+        title: const Text('Tutorials Point - clip Rect'),
       ),
-      body: const SizedBox.shrink(),
-    );
+      body: Center(
+        child: ClipRect(
+          child: Align(
+            widthFactor: 0.3,
+              heightFactor: 1.0,
+              child: Image.network(
+                'https://images.pexels.com/photos/1534411/pexels-photo-1534411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+              )
+            ),
+           ),
+          ),
+         );
   }
 }
