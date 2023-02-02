8d550e9ee4b5480fe252c520bc8a6f9eff2b3201
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-02-02 15:38:04 +0530

    changed_dimen_img

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,15 +7,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tutorials Point - clip Rect'),
+        title: const Text('Tutorials Point - After Clipping'),
       ),
       body: Center(
         child: ClipRect(
           child: Align(
-            widthFactor: 0.3,
-              heightFactor: 1.0,
+            widthFactor: 0.5,
+              heightFactor: 0.7,
               child: Image.network(
-                'https://images.pexels.com/photos/1534411/pexels-photo-1534411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+                'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
               )
             ),
            ),
