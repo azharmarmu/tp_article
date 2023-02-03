399b7b11f474d15cbbd82febcbce44dbeeeb6f4e
Azhar <marmuazhar@gmail.com>
2023-02-03 21:16:20 +0530

    article link added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # tp_article
 
 article_link: 
-https://docs.google.com/document/d/1SAOphiWMGJ5hF0qGjzCbtoR7qpFyOyg0PRFAkA546_s/edit;
+https://docs.google.com/document/d/10fZ7vBCAUu-jOoP6nNt_nZ3oiPEVS4ss3IJAmi_0e1o/edit;
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
     return Center(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('TP IconButton'),
+          title: const Text('TP InkWell'),
           backgroundColor: Colors.blue,
         ),
         body: Center(
@@ -21,42 +21,39 @@ class HomePage extends StatelessWidget {
                   'https://www.tutorialspoint.com/market/public/assets/images/logo-color.png',
                 ),
               ),
-              Container(
-                color: Colors.green,
-                child: IconButton(
-                  color: Colors.white,
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    showModalBottomSheet<void>(
-                      context: context,
-                      builder: (BuildContext ctx) {
-                        return SafeArea(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              const ListTile(
-                                leading: Icon(Icons.music_note),
-                                title: Text('Music'),
-                              ),
-                              const ListTile(
-                                leading: Icon(Icons.video_file),
-                                title: Text('Video'),
-                              ),
-                              const SizedBox(height: 24),
-                              Container(
-                                color: Colors.green,
-                                child: IconButton(
-                                  onPressed: () => Navigator.of(context).pop(),
-                                  icon: Icon(Icons.close),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
+              InkWell(
+                onTap: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (BuildContext ctx) {
+                      return SafeArea(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: <Widget>[
+                            const ListTile(
+                              leading: Icon(Icons.music_note),
+                              title: Text('Music'),
+                            ),
+                            const ListTile(
+                              leading: Icon(Icons.video_file),
+                              title: Text('Video'),
+                            ),
+                            const SizedBox(height: 24),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  color: Colors.green,
+                  child: const Text(
+                    'Click me',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ],
