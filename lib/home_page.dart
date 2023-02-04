67528db996d6e78fe0b695d24d3498a125719f19
dhivya-05 <dhivyaravichandran15@gmail.com>
2023-02-04 14:47:54 +0530

    h

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomePage());
-
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
