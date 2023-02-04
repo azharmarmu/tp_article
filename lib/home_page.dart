08ab3dbeb983c3095337bd24c3124000e9a8e3f6
Azhar <marmuazhar@gmail.com>
2023-02-04 16:19:37 +0530

    format

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,13 +8,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    int itemIndex = 0;
-    final items = [
-      const Item1(),
-      const Item2(),
-      const Item3(),
-      const Item4(),
-    ];
+  int itemIndex = 0;
+  final items = [
+    const Item1(),
+    const Item2(),
+    const Item3(),
+    const Item4(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,81 +35,93 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: customBottomBar(context),
     );
   }
-    Container customBottomBar(BuildContext context){
-      return Container(
-        height: 60,
-        decoration:const  BoxDecoration(
-          color: Colors.teal,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(onPressed: () {
+
+  Container customBottomBar(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: const BoxDecoration(
+        color: Colors.teal,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {
               setState(() {
                 itemIndex = 0;
               });
-            }, icon: itemIndex == 0 ?
-            const Icon(
-              Icons.home_filled,
-              color: Colors.white,
-              size: 35,
-            ):const Icon(
-              Icons.home_outlined,
-              color: Colors.white,
-              size: 35,
-            ),
-            ),
-            IconButton(onPressed: () {
+            },
+            icon: itemIndex == 0
+                ? const Icon(
+                    Icons.home_filled,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.home_outlined,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+          ),
+          IconButton(
+            onPressed: () {
               setState(() {
-                itemIndex= 1;
+                itemIndex = 1;
               });
-            }, icon: itemIndex == 1 ?
-            const Icon(
-              Icons.work_rounded,
-              color: Colors.white,
-              size: 35,
-            ) : const Icon(
-              Icons.work_outline_outlined,
-              color: Colors.white,
-              size: 35,
-            ),
-            ),
-            IconButton(onPressed: () {
+            },
+            icon: itemIndex == 1
+                ? const Icon(
+                    Icons.work_rounded,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.work_outline_outlined,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+          ),
+          IconButton(
+            onPressed: () {
               setState(() {
-                itemIndex= 2;
+                itemIndex = 2;
               });
-            }, icon: itemIndex == 2 ?
-            const Icon(
-              Icons.widgets_rounded,
-              color: Colors.white,
-              size: 35,
-            ) : const Icon(
-              Icons.widgets_outlined,
-              color: Colors.white,
-              size: 35,
-            ),
-            ),
-            IconButton(onPressed: () {
+            },
+            icon: itemIndex == 2
+                ? const Icon(
+                    Icons.widgets_rounded,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.widgets_outlined,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+          ),
+          IconButton(
+            onPressed: () {
               setState(() {
                 itemIndex = 3;
               });
-            }, icon: itemIndex == 3 ?
-            const Icon(
-              Icons.person,
-              color: Colors.white,
-              size: 35,
-            ) : const Icon(
-              Icons.person_outlined,
-              color: Colors.white,
-              size: 35,
-            ),
-            ),
-          ],
-        ),
-      );
-    }
+            },
+            icon: itemIndex == 3
+                ? const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.person_outlined,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
 
 class Item1 extends StatelessWidget {
   const Item1({Key? key}) : super(key: key);
@@ -120,7 +132,7 @@ class Item1 extends StatelessWidget {
       color: Colors.yellow.shade300,
       child: const Center(
         child: Text(
-              "Home Page",
+          "Home Page",
           style: TextStyle(
             color: Colors.white,
             fontSize: 45,
@@ -194,7 +206,3 @@ class Item4 extends StatelessWidget {
     );
   }
 }
-
-
-
-
