01c1573267b1a7c66539468e9b4f1f3886a70a34
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-02-04 09:00:52 +0530

    commit_ClipRRect_Widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,7 +9,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tutorials Point'),
       ),
-      body: const SizedBox.shrink(),
+      body:  Center(
+        child:Padding(
+          padding: const EdgeInsets.all(10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+          ),
+        )
+          
+      ),
     );
   }
 }
