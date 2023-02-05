72bec9d381efd1df8ed115a56260943e6470ff44
Azhar <marmuazhar@gmail.com>
2023-02-06 01:13:00 +0530

    Completed article

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tp_article
 
-article_link: https://docs.google.com/document/d/1Qc2cfbaHQCWbppD5gITMRUb-LnXzYZ6S6h8mIyNZl9g/edit?usp=sharing
+article_link: https://docs.google.com/document/d/1SS-M7x6sFIaKZ_SLXtpW48-qN4QgNyIxfI7pzK83T24/edit
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,15 +5,33 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ListView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tutorials Point'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ExpansionTile(
+          expandedCrossAxisAlignment: CrossAxisAlignment.start,
+          title: const Text('Fruits'),
           children: const <Widget>[
-            ExpansionTile(
-              title: Text('Tutorials Point- Expansion Tile Example'),
-              children: <Widget>[
-                Text('This is the expanded content of the tile'),
-              ],
+            ListTile(
+              title: Text(
+                'Apple',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Banana',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Carrot',
+                style: TextStyle(fontSize: 12),
+              ),
             ),
           ],
         ),
