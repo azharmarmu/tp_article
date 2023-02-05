7aec1da630661506a4a17fc47dadcac2f95f68bc
dhivya-05 <dhivyaravichandran15@gmail.com>
2023-02-05 21:08:44 +0530

    update article link

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: const <Widget>[
+            ExpansionTile(
+              title: Text('Tutorials Point- Expansion Tile Example'),
+              children: <Widget>[
+                Text('This is the expanded content of the tile'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
