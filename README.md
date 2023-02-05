89fe3dbf9cb5cf9d1cf77a1f128925b0ba5630f3
dhivya-05 <dhivyaravichandran15@gmail.com>
2023-02-05 19:46:59 +0530

    added readme file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tp_article
 
-article_link: 
+article_link: https://docs.google.com/document/d/1PJ_akXnhBiDq66PYWm-bqMfYPVxiRTEUgUf582IaubY/edit?usp=sharing
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController datecontroller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    datecontroller.text = "";
+  }
+
+  Widget build(BuildContext) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tutorials Point'),
-      ),
-      body: const SizedBox.shrink(),
+      appBar: AppBar(title: const Text("Tutorials Point")),
+      body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Center(
+              child: TextField(
+            controller: datecontroller,
+            decoration: const InputDecoration(
+                icon: Icon(Icons.calendar_today), labelText: "Enter Date"),
+            readOnly: true,
+            onTap: () async {
+              DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2020),
+                lastDate: DateTime(2100),
+              );
+              if (pickedDate != null) {
+                String formattedDate =
+                    DateFormat("dd-MM-yyyy").format(pickedDate);
+
+                setState(() {
+                  datecontroller.text = formattedDate.toString();
+                });
+              } else {
+                print("Not selected");
+              }
+            },
+          ))),
     );
   }
 }
