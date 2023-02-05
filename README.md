b89dfd0208181c5e59bbfdc8bc7c67a13355551b
dhivya-05 <dhivyaravichandran15@gmail.com>
2023-02-05 11:07:12 +0530

    added readme file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tp_article
 
-article_link: 
+article_link: https://docs.google.com/document/d/13qtKRVemvQLrteV8vKlKc0ZjXsIPw59ZwlyVVKCUosc/edit?usp=sharing
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const HomePage());
+
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
+
+  static const String _title = 'Tutorials Point';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tutorials Point'),
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatelessWidget(),
       ),
-      body: const SizedBox.shrink(),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Name',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Age',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Gender',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('John')),
+            DataCell(Text('25')),
+            DataCell(Text('Male')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Jane')),
+            DataCell(Text('30')),
+            DataCell(Text('Female')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Jim')),
+            DataCell(Text('35')),
+            DataCell(Text('Male')),
+          ],
+        ),
+      ],
     );
   }
 }
