3a379b3110e119a92549907650768427870b0f40
dhivya-05 <dhivyaravichandran15@gmail.com>
2023-02-05 12:46:09 +0530

    added readme file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tp_article
 
-article_link: 
+article_link: https://docs.google.com/document/d/1JlyBPFEi0tBuo35otzv3X4ZfHZwaDnFJdsbDxZD3QIw/edit?usp=sharing
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tutorials Point'),
+      body: const Center(
+        child: Text('Tutorials Point'),
       ),
-      body: const SizedBox.shrink(),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.add),
+            label: 'Add',
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.edit),
+            label: 'Edit',
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.delete),
+            label: 'Delete',
+          ),
+        ],
+      ),
     );
   }
 }
