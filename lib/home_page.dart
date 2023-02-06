ea787eee5a218be1ed9cef0ec40e598d1fa62eb4
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2023-02-06 18:14:13 +0530

    animated_opacity_created

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    bool _visible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tutorials Point'),
+        title: const Text('Fading a Widget'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: const SizedBox.shrink(),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0: 0.0,
+          duration:const Duration(milliseconds: 500),
+          child:Container(
+            width: 300,
+            height: 300,
+            color:Colors.deepPurpleAccent
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        onPressed: (){
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: const Icon(Icons.flip),
+      ),
+
     );
+
   }
 }
+
